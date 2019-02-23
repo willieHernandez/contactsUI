65211df85f0435ef3a60520342962dc9537ce3e8
willieHernandez <guillermo.w.hernandezjr@gmail.com>
2019-02-23 09:27:56 -0600

    Basic config of the testing framework and application

---
+++ src/components/PhoneBookHome.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+
+class PhoneBookHome extends Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Contacts Application Home Page</h1>
+                {/* Create a nav bar that gives the user the ability to view phone book, add a person, edit the phonebook, delete a person*/}
+
+                {/* Give a search bar to allow them to search for a contact from the home page. */}
+
+                {/* Give an overview of the phone book and cover
+                    how many contacts they have
+                    contacts by type */}
+
+            </div>
+        )
+    }
+}
+
+
+export default PhoneBookHome;
